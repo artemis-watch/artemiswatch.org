8b2797ae1ebf4eaa83e071bec31d52e8fa492c9a
Josh Terrill <josh@hacked.codes>
2026-04-18 15:22:08 -0400

    fixed dev mode

---
+++ browser-sync.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  server: {
+    baseDir: ['src', '.', 'dist'],
+    index: 'index.html',
+  },
+  files: ['src/**/*', 'images/**/*', 'favicon.ico', 'dist/style.css'],
+  open: false,
+  notify: false,
+};
